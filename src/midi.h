0cb0542db95be4dbdcf0cfb409b4a0be2517f705
matheustomazella <matheus250504@gmail.com>
2024-05-05 15:23:52 -0300

    MIDI start

---
+++ src/midi.h
@@ -0,0 +1,15 @@
+#include "MIDIUSB.h"
+
+void noteOn(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+  MidiUSB.flush();
+}
+
+void noteOff(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+  MidiUSB.flush();
+}
